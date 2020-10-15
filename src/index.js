43eb213cc80460989ea0f96dd93833f561368170
TatyanaBojarkina <galaktozat@yandex.ru>
2020-10-15 14:39:49 +0400

    init: start task Signed-off-by: TatyanaBojarkina <galaktozat@yandex.ru>

--- src/index.js
+++ src/index.js
@@ -2,5 +2,36 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+	if(typeof matrix != 'object')
+	{
+		return [];
+	}
+	if(matrix.length == 0)
+	{
+		return [];
+	}
+	var arr = [];
+	for(let i = 0; i < matrix.length; i++)
+	{
+		if(i%2 == 0)
+		{
+			matrix[i].sort(compareNumbers);
+		}
+		else{
+			matrix[i].sort(compareNumbers_1);
+		}
+		
+		for(let j = 0; j < matrix[i].length; j++)
+		{
+			arr.push(matrix[i][j]);
+		}
+	}
+	
+  return arr;
+}
+function compareNumbers(a, b) {
+  return a - b;
+}
+function compareNumbers_1(a, b) {
+  return b - a;
 }
